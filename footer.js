c7a985d0bddbead33940979721b8ec84fabce63a
quasar1742 <73203605+quasar1742@users.noreply.github.com>
2023-01-09 21:36:25 -0800

    changed year to 2023

--- footer.js
+++ footer.js
@@ -6,8 +6,8 @@ document.write(
     <div class="row-container footer">
         <div class="row">
             <div class="column" >
-                <p>© 2019 WHS Robotics. All rights reserved.  LEGO is a registered trademark of the LEGO Group. © 2019 The LEGO Group. All rights reserved.  
-                    VEX and VEX Robotics are trademarks or service marks of Innovation First International, Inc. Copyright © 2002-2019. All Rights Reserved.  
+                <p>© 2023 WHS Robotics. All rights reserved.  LEGO is a registered trademark of the LEGO Group. © 2023 The LEGO Group. All rights reserved.  
+                    VEX and VEX Robotics are trademarks or service marks of Innovation First International, Inc. Copyright © 2002-2023. All Rights Reserved.  
                     All other product names/marks are the property of their respective owners. </p>
                 <a href="https://www.iubenda.com/privacy-policy/20548363" class="iubenda-white iubenda-embed" title="Privacy Policy " >Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
             </div>
